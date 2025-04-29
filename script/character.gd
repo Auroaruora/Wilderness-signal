3c79237520f4b479382405b0e04590ee949ca61a
Auroaruora <zw2985@columbia.edu>
2025-04-29 13:23:38 -0400

    Revert "adding background sound"
    This reverts commit 058cf1aeed8371f295803b2981839ba715679db4.

--- script/character.gd
+++ script/character.gd
@@ -60,9 +60,8 @@ func _ready() -> void:
 func _unhandled_input(event: InputEvent) -> void:
 	# Handle action inputs
 	if event.is_action_pressed("interact") and inventory.get_selected_item_name()=="axe":
-		attempt_action("axe")
-	if event.is_action_pressed("interact") and inventory.get_selected_item_name()=="pickaxe":
-		attempt_action("pickaxe") 
+		#and event.button_index == MOUSE_BUTTON_LEFT and event.pressed
+		attempt_action("axe")  # You can extend this to multiple actions easily
 		
 func _physics_process(delta: float) -> void:
 	# Check health status first - only process movement if alive
@@ -108,11 +107,6 @@ func setup_action_handlers() -> void:
 		"hammer",
 		func(): print("Hammering action"),  # This will be overridden by the tower
 		"hammer_"  # Animation prefix
-	)
-	add_action(
-		"pickaxe",
-		func(): print("pickaxing action"),  # This will be overridden by the tower
-		"pickaxe_"  # Animation prefix
 	)
 
 func add_action(action_name: String, action: Callable, animation_prefix: String) -> void:
