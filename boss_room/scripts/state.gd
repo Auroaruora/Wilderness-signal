5da5acf09e7efc9703a525e276db1d8ee8139950
yw4261 <yw4261@columbia.edu>
2025-05-09 15:09:34 -0400

    change bgm

--- boss_room/scripts/state.gd
+++ boss_room/scripts/state.gd
@@ -2,7 +2,6 @@ extends Node2D
 
 class_name State
 
-@onready var debug = owner.find_child("debug")
 @onready var player = owner.get_parent().find_child("Character")
 @onready var animation_player = owner.find_child("AnimationPlayer")
 
@@ -20,4 +19,3 @@ func transition():
  
 func _physics_process(_delta):
 	transition()
-	debug.text = name
